6270023441256feca8889f1042546429e2086738
Denyon.Domingos <deny_domin@hotmail.com>
2017-03-24 15:58:37 -0300

    Include Animals and show

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do |i|
-  animal_breed = Faker::Name.animal_breed
-
+100.times do |i|
+  animal_breed = Faker::Name.dog
+  animal_genre = Faker::Name.genre
   animal_castrated = Faker::Boolean.boolean
   animal_birth = Faker::Date.birthday(18, 65)
-
+  animal_name = Faker::Cat.name
   animal_nameowner = Faker::Name.name
-  animal_phoneowner = Faker::PhoneNumber.phone_number
+  animal_phoneowner = Faker::PhoneNumber.cell_phone
   animal_lastvisit = Faker::Date.between_except(1.year.ago, 1.year.from_now, Date.today)
 
-  puts "Cadastrando Animal #{animal_nameowner}"
-  Animal.create(breed: animal_breed,
+  puts "Cadastrando Animal #{i+1}"
+  Animal.create(breed: animal_breed, genre: animal_genre,
                 castrated: animal_castrated, birth: animal_birth,
-                nameowner: animal_nameowner,
+                name: animal_name, nameowner: animal_nameowner,
                 phoneowner: animal_phoneowner, lastvisit: animal_lastvisit)
 end
